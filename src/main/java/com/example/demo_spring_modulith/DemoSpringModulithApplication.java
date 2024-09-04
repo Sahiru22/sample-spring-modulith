eaa0600c4a704d4ef361b2fb37ccea5c8dfa2bac
Sahiru22 <sahirubalasuriya222@gmail.com>
2024-09-04 11:56:25 +0530

    initial commit

---
+++ src/main/java/com/example/demo_spring_modulith/DemoSpringModulithApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_spring_modulith/DemoSpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
